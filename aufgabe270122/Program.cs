6c6c48f803e86c97e87e5f020476933fd8fd9444
AngeloGrabner <angelo.grabner@gmx.de>
2022-02-02 14:37:56 +0100

    still debuging and building the system

--- aufgabe270122/Program.cs
+++ aufgabe270122/Program.cs
@@ -6,18 +6,17 @@ namespace Program
 {
     public static class Program
     {
-        static int findProfitsFirstIndex(string input)
-        {
-            char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-            int output;
-            for (int i = 0; i < 10; i++)
-            {
-                output = findNthof(input, numbers[i], 6);
-                if (output != -1)
-                    return output;
-            }
-            return -1;
-        }
+        //static int findProfitsFirstIndex(string input)
+        //{
+        //    int[] found = new int[10];
+        //    char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        found[i] = Array.IndexOf<char>(input.ToCharArray(), numbers[i]);
+        //    }
+
+        //    return 0;
+        //}
         static int findNthof(char[] input, char c, int n = 1)
         {
             return findNthof(input.ToString(), c, n);
@@ -54,19 +53,14 @@ namespace Program
         {
             double dNumber = 0;
             char[] profits;
-            int profitNumberStartsAt = 0;
+            const int profitNumberStartsAt = 20;
             string[] inputText;
             while (true)
             {
                 Console.WriteLine("geben sie den pfard zur datei ein:");
                 string? inputPath = @"";
                 inputPath = Console.ReadLine();
-                if (inputPath != null)
-                {
-                    Console.WriteLine("input was emty");
-                    continue;
-                }
-                else if (!File.Exists(inputPath))
+                if (!File.Exists(inputPath))
                 {
                     Console.WriteLine("file does nt exits at {0}", inputPath);
                     continue;
@@ -80,33 +74,55 @@ namespace Program
             for (int i = 1; i < inputText.Length; i++) //loops through all rows 
             {
                 //converting text to number (form 2.735,50 € to 2735.5)
-                profitNumberStartsAt = findProfitsFirstIndex(inputText[i]);
-                if (profitNumberStartsAt == -1)
-                    Console.WriteLine("error on profitNumberStartsAt");
-                profits = new char[inputText.Length - profitNumberStartsAt - 1];
-                for (int j = profitNumberStartsAt; j < inputText[i].Length; j++)
-                {
-                    profits[j-profitNumberStartsAt] = inputText[i][j];
-                }
-                int findPoint = findNthof(profits, '.'); //getting rid of the dot in the number notation
-                if (findPoint == -1)
-                    Console.WriteLine("error on finding point");
-                for (int j = findPoint; j < profits.Length-1; j++) 
-                    profits[j] = profits[j + 1];
-                profits[profits.Length-1] = ' '; //clears the '€' from the string
-                int findkomma = findNthof(profits, ',');
-                if (findkomma == -1)
-                    Console.WriteLine("error on finding komma");
-                profits[findkomma] = '.';
+                //profitNumberStartsAt = findProfitsFirstIndex(inputText[i]); coursed error | use the set location on index instedead of searching it
                 try
                 {
-                    dNumber = Convert.ToDouble(profits);
+                    profits = new char[inputText[i].Length - profitNumberStartsAt]; //overflow
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"overflow occured: {inputText.Length}");
+                    profits = new char[inputText[i].Length];
+                }
+                for (int j = profitNumberStartsAt; j < inputText[i].Length; j++) 
+                {
+                    profits[j - profitNumberStartsAt] = inputText[i][j]; 
+                }
+                profits[profits.Length - 1] = ' '; //clears the '€' from the string
+                //int findPoint = findNthof(profits, '.'); 
+                int findPoint = Array.IndexOf(profits, '.'); //getting rid of the dot in the number notation
+                if (findPoint == -1)
+                    Console.WriteLine("couldn't find a point");
+                for (int j = findPoint-1; j < profits.Length-1; j++) 
+                    profits[j] = profits[j + 1];
+                //int findkomma = findNthof(profits, ',');
+                bool changedKomma = false;
+                for (int j = 0;j< profits.Length; j++)
+                {
+                    if (profits[j] == ',')
+                    {
+                        profits[j] = '.';
+                        changedKomma = true;
+                        //for debug
+                        Console.Write("verarbeitet: ");
+                        for (int k = 0; k < profits.Length; k++)
+                            Console.Write(profits[k]);
+                        Console.WriteLine();
+                        break;
+                    }
+                }
+                if (!changedKomma)
+                    Console.WriteLine("error: didn't find komma");
+                try
+                {
+                    dNumber = Convert.ToDouble(profits.ToString());
                 }
                 catch (FormatException)
                 {
                     Console.WriteLine("error on converting to double");
+                    Console.WriteLine($"dNumber = {dNumber}, i = {i}");
                 }
-                Console.WriteLine($"dNumber = {dNumber}, i = {i}");
+                
             }
 
             Console.ReadLine();
